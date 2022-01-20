8d5e6d589ae52f70400244f2f2b7679ce8b6d510
Ion <bajerean.ion@gmail.com>
2022-01-20 15:31:35 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+let n = 5;
+
+switch (n) {
+  case 0:
+    alert( 'Zero ' );
+    break;
+  case 1:
+    alert( 'Unu' );
+    break;
+  case 2:
+    alert( 'Doi' );
+    break;
+  case 3:
+    alert( 'Trei' );
+    break;5
+  case 4:
+    alert( 'Patru' );
+    break;
+  case 5:
+    alert( 'Cinci' );
+    break;
+  case 6:
+    alert( 'Sase' );5
+    break;
+  case 7:
+    alert( 'Sapte' );
+    break;
+  case 8:
+    alert( 'Opt' );
+    break;
+  case 9:
+    alert( 'Noua' );
+    break;
+ default:
+   alert( "Introduceti un numar de la 0 pana la 9" );
+}
