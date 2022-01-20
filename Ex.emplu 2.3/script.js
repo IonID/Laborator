4855d29dfcece77e795dee734a3542d2412810aa
Ion <bajerean.ion@gmail.com>
2022-01-20 17:05:04 +0200

    Exemplu 2.3

---
+++ Ex.emplu 2.3/script.js
@@ -0,0 +1,8 @@
+
+var a = prompt('Introdu A'),
+    b =  prompt('Introdu B'),
+    c =  prompt('Introdu C');
+ 
+var max = Math.max(a,b,c),
+    min = Math.min(a,b,c);
+alert(`Numarul Maxim este: ${max} si numarul Minim este: ${min}`);
