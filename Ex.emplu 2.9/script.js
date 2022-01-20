81232aafd94a1e7b44733c388933886fcaedee88
Ion <bajerean.ion@gmail.com>
2022-01-20 18:29:30 +0200

    Exemplu 2.9

---
+++ Ex.emplu 2.9/script.js
@@ -0,0 +1,8 @@
+const name = prompt("Cum te numesti?");
+      alert(`Salut ${name}`);
+
+if(confirm("Cunosti limbajul de programare JS?")){
+    alert("Bravo!")
+} else{
+    alert("Poti incepe chiar acum");
+}
