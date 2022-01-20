3aa65a339de85e2692adc3d8b1e96d1abe1893a0
Ion <bajerean.ion@gmail.com>
2022-01-20 19:17:57 +0200

    Exemplu 2.7

---
+++ Ex.emplu 2.7/script.js
@@ -0,0 +1,7 @@
+var age=prompt('Introduce-ti varsta');
+if (!(age >= 14 && age <= 90)) 
+  alert('Nu aveti varsta cuprinsa intre 14 si 90');
+else if (age < 14 || age > 90)
+ alert();
+  else 
+  alert('Vartsa d-stra este intre 14 si 90');
