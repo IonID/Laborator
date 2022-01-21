dd9d60da30924c548981e0829101e702e5ef991f
Ion <bajerean.ion@gmail.com>
2022-01-21 19:37:24 +0200

    Exemplu 3.3

--- Ex.emplu 3.3/script.js
+++ Ex.emplu 3.3/script.js
@@ -8,11 +8,7 @@ let styles = ["Jazz", "Blues"];         // Creem masivul
     styles.splice(1, 1, "Clasica");     //Inlocuim valoarea in mijloc
         console.log(styles);
 
-    //styles.shift();                     //Stergem primiul element din masiv
-    //   console.log(styles);   
-        
-    delete styles[0];                     //Stergem primiul element din masiv
-        console.log(styles); 
+    alert(styles.shift());              //Stergem primiul element din masiv si il afisam   
 
     styles.unshift("Reggae", "Rap");    //Adaugam 2 elemente la inceput de masiv
         console.log(styles);
