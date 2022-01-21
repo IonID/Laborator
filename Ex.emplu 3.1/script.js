12defa5ff794a4894921d9a74e14eba1585d34d7
Ion <bajerean.ion@gmail.com>
2022-01-21 19:08:55 +0200

    Exemplu 3.1

---
+++ Ex.emplu 3.1/script.js
@@ -0,0 +1,18 @@
+                                
+let styles = ["Jazz", "Blues"];         // Creem masivul
+        console.log(styles);
+        
+    styles.push("Rock-and-Roll");        //Adaugam la sfirsitul masivului
+        console.log(styles);
+
+    styles.splice(1, 1, "Clasica");     //Inlocuim valoarea in mijloc
+        console.log(styles);
+
+    //styles.shift();                     //Stergem primiul element din masiv
+    //   console.log(styles);   
+        
+    delete styles[0];                     //Stergem primiul element din masiv
+        console.log(styles); 
+
+    styles.unshift("Reggae", "Rap");    //Adaugam 2 elemente la inceput de masiv
+        console.log(styles);
